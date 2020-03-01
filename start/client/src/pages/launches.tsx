7424477b23a1ebbd29d0bff518ba38ba00dd390b
eric-kovshovik <eric.kovshovik@mx.com>
2020-03-01 13:46:29 -0700

    Add fetching from React

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,12 +1,11 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { LaunchTile, Header, Button, Loading } from '../components';
+import { RouteComponentProps } from '@reach/router';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
 interface LaunchesProps extends RouteComponentProps {}
-
-const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -24,8 +23,8 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
-export const GET_LAUNCHES = gql`
-  query GetLaunchList($after: String) {
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
     launches(after: $after) {
       cursor
       hasMore
@@ -37,7 +36,64 @@ export const GET_LAUNCHES = gql`
   ${LAUNCH_TILE_DATA}
 `;
 
+interface LaunchesProps extends RouteComponentProps {}
 
+const Launches: React.FC<LaunchesProps> = () => {
+  const {
+    data,
+    loading,
+    error,
+    fetchMore
+  } = useQuery<
+      GetLaunchListTypes.GetLaunchList,
+      GetLaunchListTypes.GetLaunchListVariables
+      >(GET_LAUNCHES);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+      <Fragment>
+        <Header />
+        {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+            <LaunchTile key={launch.id} launch={launch} />
+        ))}
+        {data.launches &&
+        data.launches.hasMore && (
+            <Button
+                onClick={() =>
+
+                    fetchMore({
+                      variables: {
+                        after: data.launches.cursor,
+                      },
+
+                      updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                          ...fetchMoreResult,
+                          launches: {
+                            ...fetchMoreResult.launches,
+                            launches: [
+                              ...prev.launches.launches,
+                              ...fetchMoreResult.launches.launches,
+                            ],
+                          },
+                        };
+                      },
+                    })
+                }
+            >
+              Load More
+            </Button>
+        )
+        }
+      </Fragment>
+  );
+}
 
 export default Launches;
 
