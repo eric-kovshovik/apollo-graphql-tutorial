0984d88135e810b5560c90f4ccdf61b321b472be
eric-kovshovik <eric.kovshovik@mx.com>
2020-03-01 13:16:03 -0700

    Connect Client to React

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,43 @@
 import React, { Fragment }  from 'react';
 import { RouteComponentProps } from '@reach/router';
+import gql from 'graphql-tag';
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
 }
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
+
 
 export default Launches;
 
