45a681d7ac766f6428878bcc713346ad47aed190
eric-kovshovik <eric.kovshovik@mx.com>
2020-03-03 20:37:56 -0700

    Manage local state

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,17 +1,31 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { resolvers, typeDefs } from './resolvers';
+import gql from 'graphql-tag';
 
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
 import injectStyles from './styles';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
     uri: 'http://localhost:4000/'
 });
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache,
@@ -22,6 +36,8 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
             authorization: localStorage.getItem('token'),
         },
     }),
+    typeDefs,
+    resolvers,
 });
 
 cache.writeData({
